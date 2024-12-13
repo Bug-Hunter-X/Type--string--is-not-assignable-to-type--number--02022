c682317ff8b9105bd10cd28504d6a55963de2f3c
BugHunter-X <askzenai@gmail.com>
2024-12-13 10:12:06 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,10 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+console.log(add(2, 2)); //4
+console.log(subtract(2, 2));//0
